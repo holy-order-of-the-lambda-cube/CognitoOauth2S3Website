0d19c090572fe9133668a035db5c675d36d0f009
procyon <paul@paulwberg.com>
2019-06-18 01:45:24 -0600

    Adding website bucket arn as output

--- terraform/modules/secure-site/outputs.tf
+++ terraform/modules/secure-site/outputs.tf
@@ -2,3 +2,8 @@ output "site-bucket" {
 	description = "The S3 bucket containing the secure website"
 	value       = aws_s3_bucket.secure-site.bucket
 }
+
+output "site-bucket-arn" {
+	description = "The S3 bucket ARN containing the secure website"
+	value       = aws_s3_bucket.secure-site.arn
+}
