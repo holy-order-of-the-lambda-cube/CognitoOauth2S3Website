c20779664e91db5ca39db5c534fb6ad531836f82
procyon <paul@paulwberg.com>
2019-06-25 21:00:43 -0600

    Updating to standard codebuild image

--- terraform/modules/secure-site/main.tf
+++ terraform/modules/secure-site/main.tf
@@ -305,7 +305,7 @@ resource "aws_codebuild_project" "secure-site" {
 
 	environment {
 		compute_type = "BUILD_GENERAL1_SMALL"
-		image = "aws/codebuild/nodejs:10.14.1"
+		image = "aws/codebuild/standard:2.0"
 		type = "LINUX_CONTAINER"
 
 		environment_variable {
